4df0cd85262d5a85778b252d2cb056665a441439
huangbo <huangbo@asiainfo.com>
2021-08-17 16:58:59 +0800

    单元测试优化@20210817

--- nuosi-logicflow/src/test/java/com/nuosi/flow/logic/model/domain/AttrTest.java
+++ nuosi-logicflow/src/test/java/com/nuosi/flow/logic/model/domain/AttrTest.java
@@ -1,11 +1,10 @@
-package com.nuosi.flow.logic.model;
+package com.nuosi.flow.logic.model.domain;
 
 import com.alibaba.fastjson.JSON;
 import com.nuosi.flow.data.BizDataLimitManager;
 import com.nuosi.flow.data.BizDataManager;
 import com.nuosi.flow.data.impl.BizData;
 import com.nuosi.flow.data.impl.BizDataDefine;
-import com.nuosi.flow.logic.model.element.Var;
 import com.nuosi.flow.logic.util.DtoUtil;
 import org.junit.After;
 import org.junit.Assert;
@@ -13,35 +12,32 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * <p>desc: Var标签测试</p>
+ * <p>desc: Attr标签测试</p>
  * <p>date: 2021/3/6 16:33</p>
  * @author nuosi fsofs@163.com
  * @version v1.0.0
  */
-public class VarTest {
+public class AttrTest {
 
     @Before
     public void before(){
-        BizDataDefine bizDataDefine = new BizDataDefine("Var");
+        BizDataDefine bizDataDefine = new BizDataDefine("Attr");
         bizDataDefine.defineLimit("id", BizDataLimitManager.createStringLimit());
         bizDataDefine.defineLimit("type",BizDataLimitManager.createStringLimit());
         bizDataDefine.defineLimit("name",BizDataLimitManager.createStringLimit());
-        bizDataDefine.defineLimit("final",BizDataLimitManager.createStringLimit());
-        bizDataDefine.defineLimit("default",BizDataLimitManager.createStringLimit());
-        bizDataDefine.defineLimit("initial",BizDataLimitManager.createStringLimit());
         BizDataManager.registerDto(bizDataDefine);
     }
 
     @Test
-    public void testJsonToVar(){
-        BizData varData = new BizData("Var");
+    public void testJsonToAttr(){
+        BizData varData = new BizData("Attr");
         varData.put("id","goods_name").put("type","string").put("name","货物名称");
         // 从获取Json开始验证
         String varJson = varData.toJSONString();
-        Var var = JSON.parseObject(varJson, Var.class);
-        System.out.println(var);
-        System.out.println(JSON.toJSONString(var));
-        System.out.println(DtoUtil.toString(var));
+        Attr attr = JSON.parseObject(varJson, Attr.class);
+        System.out.println(attr);
+        System.out.println(JSON.toJSONString(attr));
+        System.out.println(DtoUtil.toString(attr));
         Assert.assertTrue(true);
     }
 
